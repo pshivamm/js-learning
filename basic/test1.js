7e5b9a636e6d1c9235e7727ef1066a0526c7d28a
pshivamm <shivam.pandey63079801@gmail.com>
2024-12-05 18:31:53 +0000

    container config done

--- basic/test1.js
+++ basic/test1.js
@@ -1,1 +1,21 @@
-console.log("shivam")
+const accountId = 12345
+let accountEmail = "abc@gmail.com"
+var accountPassword = 857589
+accountName = "shivam"
+let accountState;
+
+
+// accounId = 67890 // not allowed
+
+accountEmail = "ccc@gmail.com"
+
+accountName = "newshivam"
+
+// console.log(accountName); // old method
+
+/* do not prefer to use var
+because of its block scope and functional scope
+*/
+
+// new table way
+console.table([accountId, accountEmail, accountName, accountPassword, accountState])
